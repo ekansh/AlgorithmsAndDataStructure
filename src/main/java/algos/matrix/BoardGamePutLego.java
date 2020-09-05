b1c88faa81b4e7134f38bc3cfe841d543d0b8d9c
ekansh <mail.ekansh@gmail.com>
2020-09-05 12:44:32 -0400

    added the question

--- src/main/java/algos/matrix/BoardGamePutLego.java
+++ src/main/java/algos/matrix/BoardGamePutLego.java
@@ -1,7 +1,32 @@
 package algos.matrix;
 
 import java.util.*;
+/**
+ * https://leetcode.com/discuss/interview-question/799477/Google-or-Phone-Screen-or-Reject
+ * You are implementing a board game where you are given a size N x M for the board, where N is the number of rows and M is the number of columns.
+In this board game you are playing with some fixed size lego pieces, where each player places 1 piece on the board every turn until no more piece can fit onto the board, and the last player to move wins.
 
+The problem is to implement a method for making a move on this board, placing a piece wherever there is space available, and returns a boolean indicating whether or not the player that has just made the move has won.
+
+Follow up question: The method should also find if there is any move that can be made that will make it so that the next player is unable to place a piece anywhere on the board in their next turn, and make that move.
+
+You choose how to represent the board and lego pieces in the problem, during my interview I chose to use a 2D array of characters for the board, where each X indicates occupied and O indicates spot is free and lego piece has dimension 2 X 4.
+
+Simple example (small board for demonstration purposes, board length = 4 x 4, lego piece length = 2 x 4):
+Empty Board:
+O O O O
+O O O O
+O O O O
+O O O O
+Board after player 1 makes move, placing lego piece at Middle columns:
+O X X O
+O X X O
+O X X O
+O X X O
+At this point, player 2 can't make a move and player 1 wins
+ * @author Ekansh
+ *
+ */
 public class BoardGamePutLego {
 	public static void main(String[] args) {
 		BoardGamePutLego b = new BoardGamePutLego(6, 6, 2, 2);
